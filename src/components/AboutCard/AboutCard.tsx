d8793c6ed982a805c147e3e0d80a1ad7e19301ef
carloswagner1 <cwrsilva@gmail.com>
2023-04-19 10:45:18 -0300

    update

--- src/components/AboutCard/AboutCard.tsx
+++ src/components/AboutCard/AboutCard.tsx
@@ -32,7 +32,7 @@ export default function AboutCard() {
                         O projeto foi desenvolvido em React com Typescript, utilizando o Bootstrap Framework (react-bootstrap). Para a integração com as API's disponibilizadas, foi utilizada a biblioteca Axios. O logo foi desenvolvido no GIMP.
                     </p>
                     <p>
-                        O blog é constituído de três páginas: a página 'Home' (onde apresento a solução objetivo das tarefa), a página 'Sobre o Projeto' (onde descrevo o problema, os serviços disponibilizados, a descrição da solução e as dificuldades enfrentadas) e uma página de contato (onde é possível encaminhar mensagem para meu email pessoa com críticas, sugestões, elogios ou a esperada mensagem de aprovação no processo ... kkk).
+                        O blog é constituído de três páginas: a página 'Home' (onde apresento a solução objetivo das tarefa), a página 'Sobre o Projeto' (onde descrevo o problema, os serviços disponibilizados, a descrição da solução e as dificuldades enfrentadas) e uma página de contato (onde é possível encaminhar mensagem para meu email pessoa com críticas, sugestões, elogios).
                     </p>
                     <p>
                         Observação: o formulário de contato não tem o backend eis que fora do escopo do projeto, mas está funcional utilizando o recurso do site https://formsubmit.co/.
@@ -41,7 +41,7 @@ export default function AboutCard() {
                     <p>
                         Tentei implementar a pesquisa por assunto na página de postagens, mas como os dados são mocados, com assuntos aleatórios, preferi fazer a implemantação da pesquisa por autor do texto, já que depois de um certo tempo ter conseguido exibir o nome do autor cruzando os dados obtidos nos serviços disponibilizados.
                         Mas como é mais importante o "feito que o perfeito", preferi investir no entregável.
-                        Em relação à imagem de fundo, a tela de postagem e a tela sobre o projeto rendeuram uma pesquisa para achar uma solução que contemplasse a rolagem sem perder a imagem e que não causasse conflito com a página de contatos. Por isto a implementação do MasterPage.scss.
+                        Em relação à imagem de fundo, a tela de postagem e a tela sobre o projeto renderam uma pesquisa para achar uma solução que contemplasse a rolagem sem perder a imagem e que não causasse conflito com a página de contatos. Por isto a implementação do MasterPage.scss.
                     </p>
                     </Card.Text>
                 </Card.Body>
